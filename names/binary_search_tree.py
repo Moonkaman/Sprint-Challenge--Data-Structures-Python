860a03094be24ad8e6b2f370b18b6e359276af57
Moonkaman <nickbatmanglidj@gmail.com>
2019-05-10 09:47:27 -0600

    Optimize names.py

---
+++ names/binary_search_tree.py
@@ -0,0 +1,55 @@
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value > self.value:
+            if not self.right:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
+        else:
+            if not self.left:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+
+    def contains(self, target):
+        if self.value == target:
+            return True
+        elif self.value > target:
+            if self.left:
+                return self.left.contains(target)
+            else:
+                return False
+        elif self.value < target:
+            if self.right:
+                return self.right.contains(target)
+            else:
+                return False
+
+    def get_max(self):
+        if self.right:
+            return self.right.get_max()
+        else:
+            return self.value
+
+    def for_each(self, cb):
+        if self.left:
+            self.left.for_each(cb)
+        if self.right:
+            self.right.for_each(cb)
+        self.value = cb(self.value)
+
+
+tree = BinarySearchTree(5)
+tree.insert(2)
+tree.insert(3)
+tree.insert(7)
+# print(tree.contains(4))
+
+
+def plus_two(val):
+    return val+100
--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from binary_search_tree import BinarySearchTree
 
 start_time = time.time()
 
@@ -6,17 +7,22 @@ f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+bst = BinarySearchTree(names_1[0])
+for i in range(len(names_1)):
+    if i == 0:
+        continue
+    else:
+        bst.insert(names_1[i])
+
 f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+for name in names_2:
+    if bst.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
-
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
